503c329a481796883096302f69fe438546b025a0
saba383810 <saba383810@gmail.com>
2019-07-13 21:55:52 +0900

    更新しました

---
+++ prob_2_2/minmax.cpp
@@ -0,0 +1,26 @@
+#include "minmax.h"
+ 
+//  最大値を返す
+int MinMax::max(int n1,int n2,int n3){
+    if (n1 > n2){
+		if(n1 > n3){
+			return n1;
+		}
+		else
+		{
+			return n3;
+		}
+    }
+	else if(n2 > n3)
+	{
+		return n2;
+	}
+	else
+	{
+		return n3;
+	}
+}
+//  最小値を返す
+int MinMax::min(int n1,int n2,int n3){
+    return -max(-n1, -n2, -n3);
+}
