368fc874a6fe7e447651cb4bf868afd308b7999d
saba383810 <saba383810@gmail.com>
2020-06-25 17:17:00 +0900

    commit

---
+++ hoge/yazyu.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#define クソデカ整数 long long
+#define TDN整数 int
+#define やりますねぇ 114514191981031LL
+#define ほら std
+#define 見ろよ見ろよ cout
+#define オォン endl
+#define 二人は幸せなキスをして終了 return
+TDN整数 main(void)
+{
+    クソデカ整数 a = やりますねぇ;
+    ほら::見ろよ見ろよ<< a << ほら::オォン;
+    二人は幸せなキスをして終了 0;
+}
