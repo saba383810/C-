35daf8c977cfd9bbe9fde7c94a30d6ebb398b264
saba383810 <saba383810@gmail.com>
2020-07-09 20:49:00 +0900

    commit

---
+++ algorithm/day3/test.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+    int N = 5;
+    for(int i=0;i<N;i++ )
+    {
+        if(i) cout <<i<< "で分岐に入ったよ" <<endl;
+    }
+    cout << "分岐の外だよ" <<endl;
+}
